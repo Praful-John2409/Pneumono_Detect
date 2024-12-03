06fb3df157f9417a535c19cc70aa95400577d261
SoulMind01 <87232496+SoulMind01@users.noreply.github.com>
2024-12-02 21:27:38 -0800

    Updated `setup.sh`. Upgrade `keras` and `tensorflow`

--- setup.sh
+++ setup.sh
@@ -11,5 +11,8 @@ python -m pip install --upgrade pip
 echo "Installing requirements..."
 pip install -r requirements.txt
 
+echo "Upgrading TensorFlow and Keras..."
+pip install --upgrade tensorflow keras
+
 echo "Setup complete! To activate the environment, run:"
 echo "source tf_test_env/bin/activate" 
